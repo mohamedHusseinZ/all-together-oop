60fd5e0baea7f859fa8036ca4df32828a38bf420
Rahy-Sam <raymond.mwaiki@student.moringaschool.com>
2024-01-04 07:23:36 -0800

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
-    
+
+    class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def display_info(self):
+        return f"{self.title} by {self.author}, {self.pages} pages"
+
+# Example usage:
+my_book = Book(title="The Great Gatsby", author="F. Scott Fitzgerald", pages=218)
+
+# Display information about the book
+print(my_book.display_info())
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+
+    def shoe_info(self):
+        return f"{self.brand} - Size {self.size} - Color: {self.color}"
+
+# Example usage:
+my_shoe = Shoe(brand="Nike", size=10, color="Blue")
+
+# Display information about the shoe
+print(my_shoe.shoe_info())
